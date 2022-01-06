c0c0864b0aeb1e0d19e49a438d7735aa812a14ff
Akaash <75741034+Akaash-810@users.noreply.github.com>
2022-01-06 20:03:54 +0100

    Update P_Pass_Oublier.php

--- PHP/P_Pass_Oublier.php
+++ PHP/P_Pass_Oublier.php
@@ -1,14 +1,5 @@
 <link rel="stylesheet" type="text/css" href="../CSS/Formulaire.css">
 
-
-<!--<button onclick="document.getElementById('id01').style.display='block'">Inscription</button>-->
-
-<!-- The Modal -->
-<!--<div id="id01" class="modal">-->
-
-  <!--<span onclick="document.getElementById('id01').style.display='none'" title="Close Modal">&times;</span>-->
-
-  <!-- Modal Content -->
   <form class="modal-content animate" action="/action_page.php">
     <div class="imgcontainer">
         <h1>Mot de passe oublier</h1>
@@ -22,12 +13,6 @@
 					<a href="P_Connexion.php">Retour</a>
     </div>
 
-
-
-    <!--<div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>-->
   </form>
 </div>
 
